ab0e070861613f3d2754f3b1d1c82b6aaf9e32c8
jaysashgundaya16 <jaysashgundaya17@gmail.com>
2025-03-03 06:10:22 +0800

    added spiner to favorites.tsx

--- src/pages/home-tabs/Favorites.tsx
+++ src/pages/home-tabs/Favorites.tsx
@@ -6,14 +6,9 @@ import {
   IonPage, 
   IonTitle, 
   IonToolbar,
-  IonAvatar,
-  IonLabel,
-  IonIcon,
   IonItem,
-  IonItemOptions,
-  IonItemOption,
-  IonItemSliding,
-  IonList
+  IonLabel,
+  IonSpinner
   } from '@ionic/react';
   import { pin, share, trash, call } from 'ionicons/icons';
   const Favorites: React.FC = () => {
@@ -24,79 +19,61 @@ import {
             <IonButtons slot='start'>
               <IonMenuButton></IonMenuButton>
             </IonButtons>
-            <IonTitle>Favorites</IonTitle>
+            <IonTitle>Spinner</IonTitle>
           </IonToolbar>
         </IonHeader>
         <IonContent fullscreen color = "light">
-       <IonList inset={true}>
-                 <IonItemSliding>
-                   <IonItem button={true}>
-                     <IonAvatar aria-hidden="true" slot="start">
-                       <img alt="" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
-                     </IonAvatar>
-                     <IonLabel>Ionitron</IonLabel>
-                     <IonIcon aria-hidden="true" icon={call} slot="start"></IonIcon>
-                   </IonItem>
-                   <IonItemOptions>
-         
-          <IonItemOption color="danger">Delete</IonItemOption>
-        </IonItemOptions>
-        <IonItemOptions side="start">
-          <IonItemOption color="success">Archive</IonItemOption>
-        </IonItemOptions>
-                 </IonItemSliding>
-       
-                 <IonItemSliding>
-                   <IonItem button={true}>
-                     <IonAvatar aria-hidden="true" slot="start">
-                       <img alt="" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
-                     </IonAvatar>
-                     <IonLabel>Cortana</IonLabel>
-                     <IonIcon aria-hidden="true" icon={call} slot="start"></IonIcon>
-                   </IonItem>
-                   <IonItemOptions>
-       
-          <IonItemOption color="danger">Delete</IonItemOption>
-        </IonItemOptions>
-        <IonItemOptions side="start">
-          <IonItemOption color="success">Archive</IonItemOption>
-        </IonItemOptions>
-                 </IonItemSliding>
-       
-                 <IonItemSliding>
-                   <IonItem button={true}>
-                     <IonAvatar aria-hidden="true" slot="start">
-                       <img alt="" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
-                     </IonAvatar>
-                     <IonLabel>Bender</IonLabel>
-                     <IonIcon aria-hidden="true" icon={call} slot="start"></IonIcon>
-                   </IonItem>
-                   <IonItemOptions>
-         
-          <IonItemOption color="danger">Delete</IonItemOption>
-        </IonItemOptions>
-        <IonItemOptions side="start">
-          <IonItemOption color="success">Archive</IonItemOption>
-        </IonItemOptions>
-                 </IonItemSliding>
-       
-                 <IonItemSliding>
-                   <IonItem button={true}>
-                     <IonAvatar aria-hidden="true" slot="start">
-                       <img alt="" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
-                     </IonAvatar>
-                     <IonLabel>BB-8</IonLabel>
-                     <IonIcon aria-hidden="true" icon={call} slot="start"></IonIcon>
-                   </IonItem>
-                   <IonItemOptions>
-         
-          <IonItemOption color="danger">Delete</IonItemOption>
-        </IonItemOptions>
-        <IonItemOptions side="start">
-          <IonItemOption color="success">Archive</IonItemOption>
-        </IonItemOptions>
-                 </IonItemSliding>
-               </IonList>
+        <IonItem>
+        <IonLabel>Default</IonLabel>
+        <IonSpinner></IonSpinner>
+      </IonItem>
+
+      <IonItem>
+        <IonLabel>Dots</IonLabel>
+        <IonSpinner name="dots"></IonSpinner>
+      </IonItem>
+
+      <IonItem>
+        <IonLabel>Lines</IonLabel>
+        <IonSpinner name="lines"></IonSpinner>
+      </IonItem>
+
+      <IonItem>
+        <IonLabel>Lines Small</IonLabel>
+        <IonSpinner name="lines-small"></IonSpinner>
+      </IonItem>
+
+      <IonItem>
+        <IonLabel>Lines Sharp</IonLabel>
+        <IonSpinner name="lines-sharp"></IonSpinner>
+      </IonItem>
+
+      <IonItem>
+        <IonLabel>Lines Sharp Small</IonLabel>
+        <IonSpinner name="lines-sharp-small"></IonSpinner>
+      </IonItem>
+
+      <IonItem>
+        <IonLabel>Bubbles</IonLabel>
+        <IonSpinner name="bubbles"></IonSpinner>
+      </IonItem>
+
+      <IonItem>
+        <IonLabel>Circles</IonLabel>
+        <IonSpinner name="circles"></IonSpinner>
+      </IonItem>
+
+      <IonItem>
+        <IonLabel>Circular</IonLabel>
+        <IonSpinner name="circular"></IonSpinner>
+      </IonItem>
+
+      <IonItem>
+        <IonLabel>Crescent</IonLabel>
+        <IonSpinner name="crescent"></IonSpinner>
+      </IonItem>
+    
+                 
         </IonContent>
       </IonPage>
     );
