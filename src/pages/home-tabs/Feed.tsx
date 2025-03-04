4c2aa937a35fd7c2c74ca34c5c0e2c5e53049aa3
jaysashgundaya16 <jaysashgundaya17@gmail.com>
2025-03-04 21:11:19 +0800

    added my background image on IonContent and imports to Feed.css

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,16 +1,18 @@
+import React from 'react';
 import { 
   IonButtons,
-    IonContent, 
-    IonHeader, 
-    IonMenuButton, 
-    IonPage, 
-    IonTitle, 
-    IonToolbar,
-    IonList,
-    IonAlert, 
-    IonButton
+  IonContent, 
+  IonHeader, 
+  IonMenuButton, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar,
+  IonList,
+  IonAlert, 
+  IonButton
 } from '@ionic/react';
-import { pin, share, trash, call } from 'ionicons/icons';
+import './Feed.css'; 
+
 const Feed: React.FC = () => {
   return (
     <IonPage>
@@ -23,19 +25,20 @@ const Feed: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       
-      <IonContent fullscreen color="light">
-      <IonList inset={true}> 
-      </IonList>
+      <IonContent fullscreen className="background-image">
+        <IonList inset={true}> 
+        </IonList>
+        <IonButton id="present-alert">Click Me</IonButton>
+        <IonAlert
+          trigger="present-alert"
+          header="A Short Title Is Best"
+          subHeader="A Sub Header Is Optional"
+          message="A message should be a short, complete sentence."
+          buttons={['Action']}
+        ></IonAlert>
       </IonContent>
-      <IonButton id="present-alert">Click Me</IonButton>
-      <IonAlert
-        trigger="present-alert"
-        header="A Short Title Is Best"
-        subHeader="A Sub Header Is Optional"
-        message="A message should be a short, complete sentence."
-        buttons={['Action']}
-      ></IonAlert>
     </IonPage>
   );
 };
+
 export default Feed;
